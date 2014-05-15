80fe811c638e4d30b67c1d6bf98eb529c9968b2f
Gene <Gene@xuegmatoMac-mini.local>
2014-05-15 19:09:10 +0800

    m):添加了些测试代码，还没有完成

--- XHomeDemo/XHomeDemo/view/ViewController.m
+++ XHomeDemo/XHomeDemo/view/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "RoomsViewController.h"
+#import "ScenaryViewController.h"
 
 @interface ViewController ()
 
@@ -18,6 +20,29 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIButton *roomBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    roomBtn.frame = CGRectMake(0, 100, 100, 40);
+    [roomBtn setTitle:@"Rooms" forState:UIControlStateNormal];
+    [roomBtn addTarget:self action:@selector(roomBtnClc:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:roomBtn];
+    
+    UIButton *scenaryBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    scenaryBtn.frame = CGRectMake(100, 100, 100, 40);
+    [scenaryBtn setTitle:@"Scenary" forState:UIControlStateNormal];
+    [scenaryBtn addTarget:self action:@selector(scenaryBtnClc:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:scenaryBtn];
+}
+
+- (void)roomBtnClc:(id)sender
+{
+    RoomsViewController *rvc = [[RoomsViewController alloc] init];
+    [self.navigationController pushViewController:rvc animated:YES];
+}
+
+- (void)scenaryBtnClc:(id)sender
+{
+    ScenaryViewController *svc = [[ScenaryViewController alloc] init];
+    [self.navigationController pushViewController:svc animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
