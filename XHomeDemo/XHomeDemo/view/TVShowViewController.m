f7c005ecef36f19b0d4bafc8885321798648005f
Gene <Gene@xuegmatoMac-mini.local>
2014-06-05 19:17:18 +0800

    m)：修改了遥控界面

--- XHomeDemo/XHomeDemo/view/TVShowViewController.m
+++ XHomeDemo/XHomeDemo/view/TVShowViewController.m
@@ -13,29 +13,28 @@
 @end
 
 @implementation TVShowViewController
-@synthesize scrvTV;
-@synthesize btnDown;
-@synthesize btnUP;
-@synthesize btnONOFF;
-@synthesize btn0;
-@synthesize btn1;
-@synthesize btn2;
-@synthesize btn3;
-@synthesize btn4;
-@synthesize btn5;
-@synthesize btn6;
-@synthesize btn7;
-@synthesize btn8;
-@synthesize btn9;
-@synthesize btnCustom;
-@synthesize btnGoback;
-@synthesize btnLeft;
-@synthesize btnMenu;
-@synthesize btnMute;
-@synthesize btnNULL;
-@synthesize btnOK;
-@synthesize btnRight;
-@synthesize btnSignelSource;
+//@synthesize btnDown;
+//@synthesize btnUP;
+//@synthesize btnONOFF;
+//@synthesize btn0;
+//@synthesize btn1;
+//@synthesize btn2;
+//@synthesize btn3;
+//@synthesize btn4;
+//@synthesize btn5;
+//@synthesize btn6;
+//@synthesize btn7;
+//@synthesize btn8;
+//@synthesize btn9;
+//@synthesize btnCustom;
+//@synthesize btnGoback;
+//@synthesize btnLeft;
+//@synthesize btnMenu;
+//@synthesize btnMute;
+//@synthesize btnNULL;
+//@synthesize btnOK;
+//@synthesize btnRight;
+//@synthesize btnSignelSource;
 
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -53,31 +52,53 @@
     // Do any additional setup after loading the view from its nib.
     self.view.backgroundColor = [UIColor blackColor];
     //添加一个scrollView
+    UIScrollView *scrv = [[UIScrollView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    scrv.contentSize = CGSizeMake(scrv.bounds.size.width, scrv.bounds.size.height * 2);
+    scrv.backgroundColor = [UIColor blackColor];
+    [scrv setPagingEnabled:YES];
+    [self.view addSubview:scrv];
 
-    scrvTV.contentSize = CGSizeMake(scrvTV.bounds.size.width, scrvTV.bounds.size.height * 2);
-    scrvTV.backgroundColor = [UIColor blackColor];
-    [self.view addSubview:scrvTV];
-
-    [btnONOFF setBackgroundImage:[UIImage imageNamed:@"kaiguan_400_400.png"] forState:UIControlStateNormal];
-    [btnUP setBackgroundImage:[UIImage imageNamed:@"up_400_400.png"] forState:UIControlStateNormal];
-    [btnDown setBackgroundImage:[UIImage imageNamed:@"down_400_400.png"] forState:UIControlStateNormal];
-    [btnRight setBackgroundImage:[UIImage imageNamed:@"right_400_400.png"] forState:UIControlStateNormal];
-    [btnLeft setBackgroundImage:[UIImage imageNamed:@"left_400_400.png"] forState:UIControlStateNormal];
-    [btnOK setBackgroundImage:[UIImage imageNamed:@"OK_400_400.png"] forState:UIControlStateNormal];
-    [btnSignelSource setBackgroundImage:[UIImage imageNamed:@"signalSource_400_400.png"] forState:UIControlStateNormal];
-    [btnMute setBackgroundImage:[UIImage imageNamed:@"mute_400_400.png"] forState:UIControlStateNormal];
-    [btnMenu setBackgroundImage:[UIImage imageNamed:@"menu_400_400.png"] forState:UIControlStateNormal];
-    [btnCustom setBackgroundImage:[UIImage imageNamed:@"custom_400_400.png"] forState:UIControlStateNormal];
-    [btnGoback setBackgroundImage:[UIImage imageNamed:@"goback_400_400.png"] forState:UIControlStateNormal];
-    [btnNULL setBackgroundImage:[UIImage imageNamed:@"NULL_400_400.png"] forState:UIControlStateNormal];
+//    [btnONOFF setBackgroundImage:[UIImage imageNamed:@"kaiguan_400_400.png"] forState:UIControlStateNormal];
+//    [scrv addSubview:btnONOFF];
+//    
+//    [btnUP setBackgroundImage:[UIImage imageNamed:@"up_400_400.png"] forState:UIControlStateNormal];
+//    [scrv addSubview:btnUP];
+//    
+//    [btnDown setBackgroundImage:[UIImage imageNamed:@"down_400_400.png"] forState:UIControlStateNormal];
+//    [scrv addSubview:btnDown];
+//    
+//    [btnRight setBackgroundImage:[UIImage imageNamed:@"right_400_400.png"] forState:UIControlStateNormal];
+//    [scrv addSubview:btnRight];
+//    
+//    [btnLeft setBackgroundImage:[UIImage imageNamed:@"left_400_400.png"] forState:UIControlStateNormal];
+//    [btnOK setBackgroundImage:[UIImage imageNamed:@"OK_400_400.png"] forState:UIControlStateNormal];
+//    [btnSignelSource setBackgroundImage:[UIImage imageNamed:@"signalSource_400_400.png"] forState:UIControlStateNormal];
+//    [btnMute setBackgroundImage:[UIImage imageNamed:@"mute_400_400.png"] forState:UIControlStateNormal];
+//    [btnMenu setBackgroundImage:[UIImage imageNamed:@"menu_400_400.png"] forState:UIControlStateNormal];
+//    [btnCustom setBackgroundImage:[UIImage imageNamed:@"custom_400_400.png"] forState:UIControlStateNormal];
+//    [btnGoback setBackgroundImage:[UIImage imageNamed:@"goback_400_400.png"] forState:UIControlStateNormal];
+//    [btnNULL setBackgroundImage:[UIImage imageNamed:@"NULL_400_400.png"] forState:UIControlStateNormal];
 //    [btn setBackgroundImage:[UIImage imageNamed:@"_400_400.png"] forState:UIControlStateNormal];
 //    [btn setBackgroundImage:[UIImage imageNamed:@"_400_400.png"] forState:UIControlStateNormal];
 //    [btn setBackgroundImage:[UIImage imageNamed:@"_400_400.png"] forState:UIControlStateNormal];
 //    [btn setBackgroundImage:[UIImage imageNamed:@"_400_400.png"] forState:UIControlStateNormal];
 //    [btn setBackgroundImage:[UIImage imageNamed:@"_400_400.png"] forState:UIControlStateNormal];
+    
+#define X 110/4.0
+#define BTNW  70
+#define ONPAGE_HIGHTE   583
+    UIButton *btn0 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btn0.frame = CGRectMake(X, 40 +ONPAGE_HIGHTE, BTNW, BTNW);
+    [btn0 setBackgroundImage:[UIImage imageNamed:@"0_400_400.png"] forState:UIControlStateNormal];
+    btn0.tag = 0;
+    [btn0 addTarget:self action:@selector(btnShortClick:) forControlEvents:UIControlEventTouchUpInside];
+    [scrv addSubview:btn0];
+    [scrv setContentSize:CGSizeMake(self.view.bounds.size.width, btn0.frame.origin.y + BTNW)];
     
     
 }
+
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -85,4 +106,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (void)btnShortClick:(UIButton *)sender
+{
+    NSLog(@"ss");
+  
+
+}
 @end
